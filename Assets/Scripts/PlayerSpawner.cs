83958e8e2c8ecf66987899aa808cc039974972a7
logan-bennion <57731241+logan-bennion@users.noreply.github.com>
2024-04-20 14:22:33 -0600

    Spawn Works now

--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Constants;
 
 public class PlayerSpawnScript : MonoBehaviour
 {
-
+    public static Vector3 sP1 = new Vector3(50f,40f,-1f);
+    public static Vector3 sP4 = new Vector3(40f,-50f,-1f);
+    public static Vector3 sP3 = new Vector3(-50f,-40f,-1f);
+    public static Vector3 sP2 = new Vector3(-40f,50f,-1f);
     public GameObject player;
+    public static Vector3[] spawnPointArray = {sP1, sP2, sP3, sP4};
+    public Vector3 spawnPoint;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +22,9 @@ public class PlayerSpawnScript : MonoBehaviour
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.P)){
-            Instantiate(player, transform.position, transform.rotation);
+            spawnPoint = spawnPointArray[Constants.Constants.playerIndex];
+            Instantiate(player, spawnPoint, transform.rotation);
+            Constants.Constants.playerIndex++;
         }
     }
 }
