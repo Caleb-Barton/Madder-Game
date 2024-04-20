d12efe5a182ee724fe0b1b52b9ce6207b8ab987f
logan-bennion <57731241+logan-bennion@users.noreply.github.com>
2024-04-20 15:18:56 -0600

    Player Objects

---
+++ Assets/Scripts/player_4.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_4 : MonoBehaviour
+{
+    public int wins = 0;
+    public static Vector3 spawnPoint = new Vector3(50f,40f,-1f);
+    public static Vector3 goalPoint = new Vector3(-50f,-40f,-1f);
+    public string playerName = "Player 4";
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
